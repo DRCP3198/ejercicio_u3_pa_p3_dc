b06b69db216812f32581776cd13acdba73035480
Dillan Coloma <renatocoloma2010@gmail.com>
2023-02-17 19:53:02 -0500

    Actualizacion

--- src/main/java/com/example/demo/service/FacturaServiceImpl.java
+++ src/main/java/com/example/demo/service/FacturaServiceImpl.java
@@ -1,5 +1,23 @@
 package com.example.demo.service;
 
-public class FacturaServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.modelo.dto.FacturaDTO;
+import com.example.demo.repository.IFacturaRepo;
+
+@Service
+public class FacturaServiceImpl implements IFacturaService{
+
+	@Autowired
+	private IFacturaRepo facturaRepo;
+	@Override
+	public void agregarFactura(FacturaDTO factura) {
+		// TODO Auto-generated method stub
+		this.facturaRepo.agregarFactura(factura);
+	}
+	
+	
+	
 
 }
--- src/main/java/com/example/demo/service/IFacturaService.java
+++ src/main/java/com/example/demo/service/IFacturaService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.modelo.dto.FacturaDTO;
+
 public interface IFacturaService {
+	
+	public void agregarFactura(FacturaDTO factura);
 
 }
